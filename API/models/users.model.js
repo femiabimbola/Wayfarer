c91ab67f8dd7b63cbca5ca31392286d7d88e851b
femi <femi.abimbola@emmakulate.com>
2019-07-04 16:11:48 +0100

    folder-setup

---
+++ API/models/users.model.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id:1,
+        firstName: 'Peters',
+        lastName: 'Michael',
+        email: 'pet@mail.com',
+        password:'peteruomo',
+        address: '5, Ring Road',
+        isAdmin: false
+    },
+    {
+        id:2,
+        firstName: 'Damilare',
+        lastName: 'Ojewale',
+        email: 'datoje@mail.com',
+        password:'damidaoteji',
+        address: '5, Ring Road',
+        isAdmin: false
+    },
+
+
+];
+
+export default users
